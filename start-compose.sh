246e021c35d15d0c89f9fbf5cf100d40fea6cc0b
Max Medovshchikov <chemmalion@gmail.com>
2025-08-09 23:55:48 +0300

    Update start-compose.sh

--- start-compose.sh
+++ start-compose.sh
@@ -10,5 +10,5 @@ MASTER_KEY=$(jq -r '."tbot-master-key"' <<<"$SECRET_JSON")
 TBOT_ALLOWED_USER_IDS=$(jq -r '."tbot-allowed-user-ids"' <<<"$SECRET_JSON")
 
 export BOT_TOKEN MASTER_KEY TBOT_ALLOWED_USER_IDS
-exec docker-compose up -d
+exec docker compose up -d
 
